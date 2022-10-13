f70765d0f6225726d5ba25db71d91e0624d415d2
Akagitsune <33925190+Akagitsunee@users.noreply.github.com>
2022-10-13 19:20:27 +0200

    fix start script

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
-if [ $# -eq 0 ]; then
-    docker-compose --project-directory docker up web gui --build -d
+if [[ $# -eq 0 ]]; then
+    docker compose --project-directory docker up web gui --build -d
 fi
 
-if [ $1 = "test" ]; then
-    docker-compose --project-directory docker up test --build
+if [[ $1 = "test" ]]; then
+    docker compose --project-directory docker up test --build
 fi
 
-if [ $1 = "web"  ]; then
-    docker-compose --project-directory docker up web --build -d
+if [[ $1 = "web"  ]]; then
+    docker compose --project-directory docker up web --build -d
 fi
 
-if [ $1 = "gui"  ]; then
-    docker-compose --project-directory docker up gui --build -d
+if [[ $1 = "gui"  ]]; then
+    docker compose --project-directory docker up gui --build -d
 fi
